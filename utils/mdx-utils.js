1ddce0ef950cf36674aab09d1484da83c40387a1
Julio-Lopes <juliocrlopes4@gmail.com>
2024-11-14 14:49:26 -0300

    first commit

---
+++ utils/mdx-utils.js
@@ -0,0 +1,20 @@
+import { api } from '../services/api'
+
+export const getPosts = async () => {
+    const {data} = await api.get('/posts'); 
+    if(data){
+        return data;
+    }
+
+    return []
+}
+
+export const getPostBySlug = async (id) => {
+    const {data} = await api.get(`/posts?select=*&id=eq.${id}`);
+
+    if(data){
+        return data;
+    }
+
+    return {}
+}
